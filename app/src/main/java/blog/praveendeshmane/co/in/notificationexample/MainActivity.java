e608ca8e92057b98c970b3914b2b3d67d625c965
praveendeshmane <deshmane.praveen@gmail.com>
2017-12-14 00:18:50 +0530

    Initial commit

---
+++ app/src/main/java/blog/praveendeshmane/co/in/notificationexample/MainActivity.java
@@ -0,0 +1,42 @@
+package blog.praveendeshmane.co.in.notificationexample;
+
+import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.content.Context;
+import android.content.Intent;
+import android.graphics.Color;
+import android.media.RingtoneManager;
+import android.os.Bundle;
+import android.support.v4.app.NotificationCompat;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void onClick(View view){
+        long[] pattern = {500,500,500,500,500,500,500};
+        NotificationCompat.Builder builder =
+                new NotificationCompat.Builder(this)
+                        .setSmallIcon(R.drawable.ic_launcher_background)
+                        .setContentTitle("Notification Example")
+                        .setSound(RingtoneManager.getDefaultUri(RingtoneManager.TYPE_NOTIFICATION))
+                        .setLights(Color.BLUE, 500, 500)
+                        .setVibrate(pattern)
+                        .setContentText("This is a test notification");
+
+        Intent notificationIntent = new Intent(this, SecondActivity.class);
+        PendingIntent contentIntent = PendingIntent.getActivity(this, 0, notificationIntent,
+                PendingIntent.FLAG_UPDATE_CURRENT);
+        builder.setContentIntent(contentIntent);
+
+        // Add as notification
+        NotificationManager manager = (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
+        manager.notify(0, builder.build());
+    }
+}
---
+++ app/src/main/java/blog/praveendeshmane/co/in/notificationexample/SecondActivity.java
@@ -0,0 +1,13 @@
+package blog.praveendeshmane.co.in.notificationexample;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class SecondActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_second);
+    }
+}
